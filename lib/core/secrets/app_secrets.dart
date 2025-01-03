8c89abe568648c6377a1951e8540a1cb0d010f87
hossain_arif <arif134221@gmail.com>
2025-01-03 20:13:49 +0600

    Implement core file

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+
+  static const supabaseUrl = 'https://iqpocttidkttojixfxlu.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxcG9jdHRpZGt0dG9qaXhmeGx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NDEzMzksImV4cCI6MjA1MTQxNzMzOX0.8L3cka8PAKW_7Lk7ksk3DWK_gXnjObk4cSbdIHMoQcQ';
+}
