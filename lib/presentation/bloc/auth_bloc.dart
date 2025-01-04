affcd019ca450b1e78eb34d9ff692f977a804ccd
hossain_arif <arif134221@gmail.com>
2025-01-04 22:33:29 +0600

    Update bloc logic

--- lib/presentation/bloc/auth_bloc.dart
+++ lib/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,8 @@
+import 'package:blog_app/domain/entities/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../features/auth/data/usecases/user_login.dart';
 import '../../features/auth/data/usecases/user_sign_up.dart';
 
 part 'auth_event.dart';
@@ -8,18 +10,23 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
+  final UserLogin _userLogin;
 
   AuthBloc({
-    required UserSignUp userSignUp
+    required UserSignUp userSignUp,
+    required UserLogin userLogin,
 }) : _userSignUp = userSignUp,
+     _userLogin = userLogin,
         super(AuthInitial()) {
     on<AuthSignUp>(_onAuthSignUp);
+    on<AuthLogin>(_onAuthLogin);
   }
 
   void _onAuthSignUp(
       AuthSignUp event,
       Emitter<AuthState> emit,
       ) async {
+    emit(AuthLoading());
     final res = await _userSignUp(
       UserSignUpParams(
         email: event.email,
@@ -30,7 +37,26 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
     res.fold(
           (failure) => emit(AuthFailure(failure.message)),
-          (uid) => emit(AuthSuccess(uid)),
+          (user) => emit(AuthSuccess(user)),
     );
   }
+
+  void _onAuthLogin(
+      AuthLogin event,
+      Emitter<AuthState> emit,
+      ) async {
+    emit(AuthLoading());
+    final res = await _userLogin(
+      UserLoginParams(
+        email: event.email,
+        password: event.password,
+      ),
+    );
+
+    res.fold(
+          (l) => emit(AuthFailure(l.message)),
+          (r) => emit(AuthSuccess(r)),
+    );
+  }
+
 }
--- lib/presentation/bloc/auth_event.dart
+++ lib/presentation/bloc/auth_event.dart
@@ -16,3 +16,13 @@ final class AuthSignUp extends AuthEvent {
   });
 }
 
+final class AuthLogin extends AuthEvent {
+  final String email;
+  final String password;
+
+  AuthLogin({
+    required this.email,
+    required this.password,
+  });
+}
+
