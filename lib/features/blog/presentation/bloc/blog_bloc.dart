7b3ef2ec4b94c09aa5e514e356e1598f93072d99
hossain_arif <arif134221@gmail.com>
2025-01-11 23:08:15 +0600

    add bloc in blog screen

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/usecase/uoload_blog.dart';
+
+
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState> {
+  final UploadBlog _uploadBlog;
+
+  BlogBloc({
+    required UploadBlog uploadBlog,
+  })  : _uploadBlog = uploadBlog,
+        super(BlogInitial()) {
+    on<BlogEvent>((event, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+  }
+
+  void _onBlogUpload(
+    BlogUpload event,
+    Emitter<BlogState> emit,
+  ) async {
+    final res = await _uploadBlog(
+      UploadBlogParams(
+        posterId: event.posterId,
+        title: event.title,
+        content: event.content,
+        image: event.image,
+        topics: event.topics,
+      ),
+    );
+
+    res.fold(
+      (l) => emit(BlogFailure(l.message)),
+      (r) => emit(BlogUploadSuccess()),
+    );
+  }
+}
