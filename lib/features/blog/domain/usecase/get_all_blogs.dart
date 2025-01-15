283ea483cac483ca681aabd9ccff2a9d44e9c6c1
hossain_arif <arif134221@gmail.com>
2025-01-15 23:55:37 +0600

    Dependency update

---
+++ lib/features/blog/domain/usecase/get_all_blogs.dart
@@ -0,0 +1,16 @@
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecase/usecase.dart';
+import '../entities/blog.dart';
+import '../repositories/blog_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+
+class GetAllBlogs implements UseCase<List<Blog>, NoParams> {
+  final BlogRepository blogRepository;
+  GetAllBlogs(this.blogRepository);
+
+  @override
+  Future<Either<Failure, List<Blog>>> call(NoParams params) async {
+    return await blogRepository.getAllBlogs();
+  }
+}
