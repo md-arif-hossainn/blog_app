fa38206910bcde8e37f7542901f585ef39c80c9b
hossain_arif <arif134221@gmail.com>
2025-01-22 12:38:36 +0600

    Update dependency injection

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,7 +1,9 @@
 
 
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/common/cubits/app_user_cubit.dart';
@@ -14,6 +16,7 @@ import 'features/auth/domain/usecases/current_user.dart';
 import 'features/auth/domain/usecases/user_login.dart';
 import 'features/auth/domain/usecases/user_sign_up.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/blog/data/datasources/blog_local_data_source.dart';
 import 'features/blog/data/datasources/blog_remote_data_source.dart';
 import 'features/blog/data/repositories/blog_repository_impl.dart';
 import 'features/blog/domain/repositories/blog_repository.dart';
@@ -21,116 +24,4 @@ import 'features/blog/domain/usecase/get_all_blogs.dart';
 import 'features/blog/domain/usecase/uoload_blog.dart';
 import 'features/blog/presentation/bloc/blog_bloc.dart';
 
-final serviceLocator = GetIt.instance;
-
-
-Future<void> initDependencies() async {
-  _initAuth();
-  _initBlog();
-
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
-
-  serviceLocator.registerLazySingleton(() => supabase.client);
-  serviceLocator.registerFactory(() => InternetConnection());
-
-  // core
-  serviceLocator.registerLazySingleton(
-        () => AppUserCubit(),
-  );
-
-  serviceLocator.registerFactory<ConnectionChecker>(
-        () => ConnectionCheckerImpl(
-      serviceLocator(),
-    ),
-  );
-
-}
-
-void _initAuth() {
-  // Datasource
-  serviceLocator
-    ..registerFactory<AuthRemoteDataSource>(
-          () => AuthRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-
-  // Repository
-  ..registerFactory<AuthRepository>(
-          () => AuthRepositoryImpl(
-        serviceLocator(),
-            serviceLocator(),
-      ),
-    )
-
-  // Usecases
-  ..registerFactory(
-          () => UserSignUp(
-        serviceLocator(),
-      ),
-    )
-
-  ..registerFactory(
-        () => UserLogin(
-      serviceLocator(),
-    ),
-  )
-    ..registerFactory(
-          () => CurrentUser(
-        serviceLocator(),
-      ),
-    )
-
-  // Bloc
- ..registerLazySingleton(
-          () => AuthBloc(
-        userSignUp: serviceLocator(),
-        userLogin: serviceLocator(),
-        currentUser: serviceLocator(),
-        appUserCubit:serviceLocator(),
-      ),
-    );
-
-}
-
-void _initBlog() {
-  // Datasource
-  serviceLocator
-    ..registerFactory<BlogRemoteDataSource>(
-          () => BlogRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-    // ..registerFactory<BlogLocalDataSource>(
-    //       () => BlogLocalDataSourceImpl(
-    //     serviceLocator(),
-    //   ),
-    // )
-  // Repository
-    ..registerFactory<BlogRepository>(
-          () => BlogRepositoryImpl(
-        serviceLocator(),
-      ),
-    )
-  // Usecases
-    ..registerFactory(
-          () => UploadBlog(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-          () => GetAllBlogs(
-        serviceLocator(),
-      ),
-    )
-  // Bloc
-    ..registerLazySingleton(
-          () => BlogBloc(
-              uploadBlog: serviceLocator(),
-              getAllBlogs: serviceLocator(),
-          ),
-    );
-}
+part 'init_dependencies.main.dart';
